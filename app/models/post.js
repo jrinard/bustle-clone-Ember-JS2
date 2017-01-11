64f5e8a09ba6015c01939bc1079c3d6ea36857ee
joshua <jrinard@hotmail.com>
2017-01-10 22:51:20 -0800

    fixed styling

--- app/models/post.js
+++ app/models/post.js
@@ -7,3 +7,5 @@ export default DS.Model.extend({
   teaser: DS.attr(),
   title: DS.attr()
 });
+// 'string', {defaultValue: 'https://typeset-beta.imgix.net/2017/1/10/474390681.jpg?w=740&h=437&fit=crop&crop=faces&auto=format&q=70'}
+// https://typeset-beta.imgix.net/2017/1/10/474390681.jpg?w=740&h=437&fit=crop&crop=faces&auto=format&q=70
